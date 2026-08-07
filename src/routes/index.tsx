6ee599c1f1bb022e02a519a259609c0fba8ef0ff
IamAristocrat <91248203+IamAristocrat@users.noreply.github.com>
2026-08-07 13:34:34 -0700

    Mount responsive premium homepage enhancements

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import heroMountains from "@/assets/hero-mountains.jpg";
 import { PublicStore } from "@/components/baristo/PublicStore";
+import { HomepageEnhancements } from "@/components/baristo/HomepageEnhancements";
+import "../experience.css";
 
 export const Route = createFileRoute("/")({
   head: () => ({
@@ -13,5 +15,14 @@ export const Route = createFileRoute("/")({
       },
     ],
   }),
-  component: PublicStore,
+  component: BaristoHomepage,
 });
+
+function BaristoHomepage() {
+  return (
+    <>
+      <PublicStore />
+      <HomepageEnhancements />
+    </>
+  );
+}
